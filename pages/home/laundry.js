9c835fd985f754630a5ae0e43df201b6bb9e3692
@cincycurtis <curtis.schumacher@gmail.com>
2021-11-26 14:02:27 -0500

    make it so

---
+++ pages/home/laundry.js
@@ -0,0 +1,5 @@
+import LocalLaundryServiceOutlinedIcon from '@mui/icons-material/LocalLaundryServiceOutlined';
+
+export default () => {
+    return <h1><LocalLaundryServiceOutlinedIcon sx={{ fontSize: 40 }}/> Laundry Room</h1>
+}
